a673dfb698a2d4490fd719e1d812e02452d57479
e.d.gerasin <e.d.gerasin@yandex.ru>
2025-06-30 19:32:08 +0300

    new Инициализация проекта

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SwagLabs_Autotests_Dotnet;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
